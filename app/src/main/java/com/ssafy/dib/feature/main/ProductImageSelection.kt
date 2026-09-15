98b79c45232b5ce2aa01145fda67072c319e5cd7
1lights1 <100738354+1lights1@users.noreply.github.com>
2026-09-15 16:12:47 +0900

    feat(product): 사진 순서 편집 구현 (#166)

--- app/src/main/java/com/ssafy/dib/feature/main/ProductImageSelection.kt
+++ app/src/main/java/com/ssafy/dib/feature/main/ProductImageSelection.kt
@@ -24,3 +24,26 @@ internal fun productImageTypeLabel(type: String): String = when (type) {
     "BOTTOM" -> "아랫면"
     else -> "방향 선택"
 }
+
+internal fun <T> moveProductImage(
+    images: MutableList<T>,
+    imageTypes: MutableList<String>,
+    fromIndex: Int,
+    toIndex: Int
+): Boolean {
+    if (images.size != imageTypes.size || fromIndex !in images.indices || toIndex !in images.indices || fromIndex == toIndex) {
+        return false
+    }
+    val image = images.removeAt(fromIndex)
+    images.add(toIndex, image)
+    val type = imageTypes.removeAt(fromIndex)
+    imageTypes.add(toIndex, type)
+
+    val newRepresentativeOriginalType = imageTypes.first()
+    imageTypes.indices.drop(1).filter { imageTypes[it] == "FRONT" }.forEachIndexed { index, position ->
+        imageTypes[position] = newRepresentativeOriginalType.takeIf { it != "FRONT" }
+            ?: secondaryProductImageTypes[index % secondaryProductImageTypes.size]
+    }
+    imageTypes[0] = "FRONT"
+    return true
+}
